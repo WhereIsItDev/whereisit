37ed23912d951de32fe3ec1426bd32064d607478
Celso Neto <celsopalmeiraneto@gmail.com>
2015-06-06 17:45:06 -0400

    Extension w/ Clicks

---
+++ Chrome Extension/injected.js
@@ -0,0 +1,10 @@
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    console.log(text);
+}
+document.addEventListener("mouseup",getSelectionText);
