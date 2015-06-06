62c4c114339ee253f960b6254de6cbe614d6e8e5
Ng Zhi An <ngzhian@gmail.com>
2015-06-06 21:41:55 +0000

    Run python script and get candidates as json string

--- gitCloner/ctags.js
+++ gitCloner/ctags.js
@@ -3,7 +3,7 @@ require ('shelljs/global');
 exports.run = function(tagname, repopath) {
   script = './scripts/runctags'
   cmd = [script, tagname, repopath].join(' ');
-  console.log(cmd);
-  var ret = exec(cmd);
-  console.log(ret);
+  var ret = exec(cmd, {silent:true});
+  candidates = JSON.parse(ret.output);
+  return candidates;
 }
