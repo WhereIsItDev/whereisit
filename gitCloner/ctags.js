f50e4d4df684247359521072c64d8802945a8183
Ng Zhi An <ngzhian@gmail.com>
2015-06-06 22:10:19 +0000

    Fix semicolons

--- gitCloner/ctags.js
+++ gitCloner/ctags.js
@@ -1,7 +1,8 @@
 require ('shelljs/global');
 
+// e.g. run('runDijkstra', 'repos/danielcodes/Algorithms/')
 exports.run = function(tagname, repopath) {
-  script = './scripts/runctags'
+  script = './scripts/runctags';
   cmd = [script, tagname, repopath].join(' ');
   var ret = exec(cmd, {silent:true});
   candidates = JSON.parse(ret.output);
