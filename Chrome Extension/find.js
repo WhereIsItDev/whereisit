0c35cfa81d1ab54d8f129390567d80d24be30e2f
Jeffrey <JeffreyLin1219@gmail.com>
2015-06-06 16:12:06 -0400

    Commit to chrome extension
    Added Chrome extension files

---
+++ Chrome Extension/find.js
@@ -0,0 +1,15 @@
+document.addEventListener('dblclick', handleClick(),console.log)
+
+function handleClick() {
+
+	//if  new request
+	createPopup(query,posX, posY, e.pageX, e.pageY);
+	//else ignore OR close current one and create a new frame
+}
+
+function createPopup(query, x, y, windowX, windowY) {
+
+	var frame = document.createElement('div');
+	frame.
+}
+
