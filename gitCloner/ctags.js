5fd5743643aa8755fd4b080e675b16cbfdc9588e
Ng Zhi An <ngzhian@gmail.com>
2015-06-07 03:34:36 +0000

    Slight escape of snippet on ctags.js

--- gitCloner/ctags.js
+++ gitCloner/ctags.js
@@ -3,6 +3,12 @@ require ('shelljs/global');
 // e.g. run('runDijkstra', 'repos/danielcodes/Algorithms/')
 exports.run = function(tagname, repopath) {
   script = './scripts/runctags';
+
+  // new lines cause the script to break
+  // we'll just read until the first newline encountered
+  tagname += '\n';
+  tagname = tagname.replace(/\n.*/, '');
+
   cmd = [script, tagname, repopath].join(' ');
   console.log('running cmd: ' + cmd);
   var ret = exec(cmd, {silent:true});
