a3de382aea2f05bc0494be1a322dd416859f1150
Jeffrey <JeffreyLin1219@gmail.com>
2015-06-06 20:39:30 -0400

    Added post and get request

--- Chrome Extension/find.js
+++ Chrome Extension/find.js
@@ -1,5 +1,30 @@
-console.log($)
+var url      = window.location.href; 
+sendrequest("methodName", url)
+//var query = file
+function sendrequest(query, url){
+	alert("Sending Request");
+	$.post(localhost,url,query,acceptedQuery())
+	// requests from the server with the query
+}
+// if returns, send data to return
+function acceptedQuery(){
 
-/*function sendrequest(){
+	alert("Acepted Query");
+	// send data to post if successful, and retrieve the URLs
+	$.get(localhost,getFromServer(data))
 
-}*/
+}
+
+function getFromServer(data){
+
+	var groupedData = data
+	for (i = 0; i < data.length; i++) { 
+
+
+	}
+
+	//send the data to the frame
+
+}
+//on localhost
+//server 8880
