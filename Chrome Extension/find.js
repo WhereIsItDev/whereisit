e2872e433a2ca12dbfc31f45798b25c28b0bf23a
Ng Zhi An <ngzhian@gmail.com>
2015-06-07 16:00:56 +0000

    Create context menu

--- Chrome Extension/find.js
+++ Chrome Extension/find.js
@@ -1,2 +1,13 @@
-var buffer = null;
+chrome.contextMenus.create({
+  title: 'Where is it?',
+  contexts: ['selection'],
+  onclick: getClickHandler()
+});
 
+function getClickHandler() {
+  return function(info ,tab) {
+    var url = chrome.extension.getURL('find.html');
+    url += '?selection=' + info.selectionText + '&location=' + info.pageUrl;
+    chrome.tabs.create({url: url});
+  }
+};
