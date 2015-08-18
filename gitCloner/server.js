06a119cb77251f345b99b51249634a32d51430e4
Ng Zhi An <ngzhian@gmail.com>
2015-08-18 14:02:25 +0000

    Fix: i broke lookup by snippet

--- gitCloner/server.js
+++ gitCloner/server.js
@@ -39,12 +39,14 @@ app.post('/', function(req,res){
     return res.sendStatus(404);
   }
 
-  if (wholeFile === true) {
+  if (url && snippet) {
+    results = ctags.run(snippet, repoPath);
+  } else if (wholeFile === true) {
     var p = url.split('/');
     filePath = p.slice(7).join('/');
     results = ctags.tag_file(filePath, repoPath);
   } else {
-    results = ctags.run(snippet, repoPath);
+    return res.json();
   }
 
   if (url && snippet) {
