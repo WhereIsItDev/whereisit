e3bdc49867756526b00ca52ac237bd3ede27f769
Ng Zhi An <ngzhian@gmail.com>
2015-08-24 03:48:12 +0000

    Fix up param names in cache

--- gitCloner/cache.js
+++ gitCloner/cache.js
@@ -8,16 +8,26 @@ var hashFn = function(url, snippet) {
   return shasum.digest('hex');
 }
 
-exports.storeUrlSnippet = function(url, snippet, response) {
+exports.storeUrlSnippet = function(url, snippet, results) {
   var cacheKey = hashFn(url, snippet);
-  cache[cacheKey] = response;
-  return response;
+  cache[cacheKey] = results;
+  return results;
 }
 
 exports.getUrlSnippet = function(url, snippet) {
-  cacheKey = hashFn(url, snippet);
+  var cacheKey = hashFn(url, snippet);
   inCache = cache[cacheKey];
   return inCache === undefined ? null : inCache;
 }
 
+exports.storeFileTags = function(filePath, repoPath, results) {
+  var cacheKey = hashFn(filePath, repoPath);
+  cache[cacheKey] = results;
+  return results;
+}
 
+exports.getFileTags = function(filePath, repoPath, results) {
+  var cacheKey = hashFn(filePath, repoPath);
+  inCache = cache[cacheKey];
+  return inCache === undefined ? null : inCache;
+}
