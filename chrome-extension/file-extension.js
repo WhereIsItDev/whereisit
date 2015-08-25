c35c11994b5231314e5013e434d66aa74e95adeb
Ng Zhi An <ngzhian@gmail.com>
2015-08-25 07:10:06 +0000

    Support java files

--- chrome-extension/file-extension.js
+++ chrome-extension/file-extension.js
@@ -1,9 +1,12 @@
 function guessFileExtension(filename) {
+  // to add a new language,
+  // add a new key-value map to mapping
   var splits = filename.split('.');
   var defaultLanguage = JavaScript;
   var mapping = {
     'py': Python,
-    'js': JavaScript
+    'js': JavaScript,
+    'java': Java
   }
 
   if (splits.length > 0) {
--- chrome-extension/language.js
+++ chrome-extension/language.js
@@ -47,3 +47,31 @@ function JavaScript() {
     isMethod: isMethod
   }
 }
+
+function Java() {
+  var methodCallRegex = /\.?\w+\(/g;
+  var methodReplaceRegex = /[.(]/g;
+
+  function matchMethodCalls(str) {
+    // method calls are usually a text node with x.method()
+    // or when there is no parenthesis, it's a class constructor
+    return str.match(methodCallRegex) || [str];
+  }
+
+  function stripMethodCall(str) {
+    return str.replace(methodReplaceRegex, '')
+  }
+
+  function isMethod(node) {
+    index = node.data.search(methodCallRegex);
+    hasClass = node.parentElement.classList.contains('pl-smi');
+    return hasClass || (index >= 0);
+  }
+
+  return {
+    name: 'Java',
+    matchMethodCalls: matchMethodCalls,
+    stripMethodCall: stripMethodCall,
+    isMethod: isMethod
+  }
+}
