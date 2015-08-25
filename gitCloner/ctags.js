9dde7b95768585b5bfda309ac48db5b8ef5dcbaa
Ng Zhi An <ngzhian@gmail.com>
2015-08-25 07:45:59 +0000

    Add logging for time to run ctags script

--- gitCloner/ctags.js
+++ gitCloner/ctags.js
@@ -3,6 +3,23 @@ require ('shelljs/global');
 var log = require('./logging');
 var emptyResult = [];
 
+function run_cmd(cmd, failResult) {
+  var start = new Date();
+  var ret = exec(cmd, {silent:true});
+
+  if (ret.code !== 0) {
+    var end = new Date();
+    log.debug('event=run_command_fail message=' + ret.output);
+    log.debug('command ' + cmd + ' took ' + ((end - start) / 1000) + 's')
+    return failResult;
+  }
+
+  var end = new Date();
+  log.debug('event=run_command_success result=' + ret.output);
+  log.debug('command ' + cmd + ' took ' + ((end - start) / 1000) + 's')
+  return JSON.parse(ret.output);
+}
+
 exports.run = function(tagname, repopath) {
   script = './scripts/runctags';
 
@@ -12,17 +29,7 @@ exports.run = function(tagname, repopath) {
   tagname = tagname.replace(/\n.*/, '');
 
   cmd = [script, tagname, repopath].join(' ');
-  log.debug('event=run_command command=' + cmd);
-
-  var ret = exec(cmd, {silent:true});
-  if (ret.code !== 0) {
-    log.debug('event=run_command_fail message=' + ret.output);
-    return emptyResult;
-  }
-
-  log.debug('event=run_command_success result=' + ret.output);
-  candidates = JSON.parse(ret.output);
-  return candidates;
+  return run_cmd(cmd, []);
 }
 
 exports.tag_file = function(filePath, repopath) {
@@ -30,16 +37,5 @@ exports.tag_file = function(filePath, repopath) {
   var codePath = [repopath, filePath].join('/');
   var cmd = [script, codePath, repopath].join(' ');
 
-  log.debug('event=run_command command=' + cmd);
-
-  var ret = exec(cmd, {silent:true});
-  if (ret.code !== 0) {
-    log.debug('event=run_command_fail message=' + ret.output);
-    return emptyResult;
-  }
-
-  log.debug('event=run_command_success result=' + ret.output);
-
-  candidates = JSON.parse(ret.output);
-  return candidates;
+  return run_cmd(cmd, []);
 }
