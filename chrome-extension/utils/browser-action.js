07c32fd49aa09035f33ff72c3a61f75275092204
Ng Zhi An <ngzhian@gmail.com>
2015-09-01 06:43:56 +0000

    Use page action badge text to indicate loading

--- chrome-extension/utils/browser-action.js
+++ chrome-extension/utils/browser-action.js
@@ -3,10 +3,12 @@ function setLoadingState() {
   chrome.browserAction.setIcon({
     path: 'img/logo19-loading.png'
   })
+  chrome.browserAction.setBadgeText({text:"?"});
 }
 
 function setNormalState() {
   chrome.browserAction.setIcon({
     path: 'img/logo19.png'
   })
+  chrome.browserAction.setBadgeText({text:""});
 }
