6bfa01943afdbeb55997ccfd47b461c08581dfaa
Ng Zhi An <ngzhian@gmail.com>
2015-09-03 03:47:01 +0000

    Add simple cache invalidation (5 mins) for web server

--- gitCloner/cache.js
+++ gitCloner/cache.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto');
 var cache = {};
+var timers = {};
 
 var hashFn = function(url, snippet) {
   var shasum = crypto.createHash('sha1');
@@ -11,23 +12,34 @@ var hashFn = function(url, snippet) {
 exports.storeUrlSnippet = function(url, snippet, results) {
   var cacheKey = hashFn(url, snippet);
   cache[cacheKey] = results;
+  timers[cacheKey] = new Date();
   return results;
 }
 
 exports.getUrlSnippet = function(url, snippet) {
   var cacheKey = hashFn(url, snippet);
   inCache = cache[cacheKey];
-  return inCache === undefined ? null : inCache;
+  return inCache === undefined ? null : isValid(cacheKey) ? inCache : null;
 }
 
 exports.storeFileTags = function(filePath, repoPath, results) {
   var cacheKey = hashFn(filePath, repoPath);
   cache[cacheKey] = results;
+  timers[cacheKey] = new Date();
   return results;
 }
 
 exports.getFileTags = function(filePath, repoPath, results) {
   var cacheKey = hashFn(filePath, repoPath);
   inCache = cache[cacheKey];
-  return inCache === undefined ? null : inCache;
+  return inCache === undefined ? null : isValid(cacheKey) ? inCache : null;
+}
+
+FIVE_MINS_IN_MS = 5 * 60 * 1000;
+function isValid(cacheKey) {
+  var timeSet = timers[cacheKey];
+  if (timeSet === undefined) return false;
+  var now = new Date();
+  var msPassed = now - timeSet;
+  return msPassed < FIVE_MINS_IN_MS;
 }
